f96814915fb06fc99cbba528fff1bcb1c60d5845
Jose Annunziato <jose.annunziato@sonos.com>
2023-03-26 09:44:57 -0400

    port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import TuiterController from "./tuiter/tuiter-controller.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
